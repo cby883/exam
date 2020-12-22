9095fd5f808175cbfd43d4cc8b7e84ae20e6b2f4
bingyi <2583150126@qq.com>
2020-12-22 12:18:58 +0800

    ++

--- src/test/java/com/chenbingyi/ExamServicesApplicationTests.java
+++ src/test/java/com/chenbingyi/ExamServicesApplicationTests.java
@@ -14,18 +14,9 @@ import java.util.List;
  */
 @SpringBootTest
 class ExamServicesApplicationTests {
-
-	@Autowired
-	LetterCombination letterCombination;
 	@Test
-	void combinationTest() {
-		List<String> expectResult=List.of("ad","ae","af","bd","be","bf","cd","ce","cf");
-		List<Integer> arr1 = Arrays.asList(2,3);
-		List<Integer> arr2=Arrays.asList(12,13);
-		List<String> result1 = letterCombination.combination(arr1);
-		List<String> result2=letterCombination.combination(arr2);
-		assert result1.toString().equals(expectResult.toString());
-		assert result2.toString().equals(expectResult.toString());
+	void contextTest() {
+
 	}
 
 }
---
+++ src/test/java/com/chenbingyi/exam/LetterCombinationTests.java
@@ -0,0 +1,28 @@
+package com.chenbingyi.exam;
+
+import com.chenbingyi.exam.LetterCombination;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ */
+@SpringBootTest
+public class LetterCombinationTests {
+    @Autowired
+    LetterCombination letterCombination;
+    @Test
+    void combinationTest() {
+        List<String> expectResult=List.of("ad","ae","af","bd","be","bf","cd","ce","cf");
+        List<Integer> arr1 = Arrays.asList(2,3);
+        List<Integer> arr2=Arrays.asList(12,13);
+        List<String> result1 = letterCombination.combination(arr1);
+        List<String> result2=letterCombination.combination(arr2);
+        assert result1.toString().equals(expectResult.toString());
+        assert result2.toString().equals(expectResult.toString());
+    }
+}
